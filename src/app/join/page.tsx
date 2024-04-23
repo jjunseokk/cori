8ff7b385faf6192908f715736064cec9b6b18a68
evan <evan@vers.kr>
2024-04-23 17:37:23 +0900

    [feat] 회원가입 유효성 검사

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -1,11 +1,31 @@
 "use client";
-import React, { ChangeEvent, FormEvent, useState } from "react"; 
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import "./join.scss";
 import { Input } from "@/component/Input/Input";
 import { Button } from "@/component/Button/Button";
+import test from "node:test";
+import axios from "axios";
+
+interface JoinFormData {
+  name: string;
+  email: string;
+  password: string;
+  checkPassword: string;
+}
+interface ModalData {
+  error: string | null;
+  type: string | null;
+}
 
 const Join = () => {
-  const [joinFrom, setJoinFrom] = useState<string[]>([]);
+  const [joinFrom, setJoinFrom] = useState<JoinFormData>({
+    name: "",
+    email: "",
+    password: "",
+    checkPassword: "",
+  });
+  const [checkPw, setCheckPw] = useState<boolean>(false);
+  const [modalContent, setModalContent] = useState<ModalData>();
 
   const handleChangeJoin = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -13,10 +33,32 @@ const Join = () => {
     setJoinFrom({ ...joinFrom, [name]: value });
   };
 
-  const handleJoin = (e: FormEvent<HTMLFormElement>) =>{
+  const handleJoin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Submitted Form Data:", joinFrom);
-  }
+
+    const guardPw = joinFrom.password === joinFrom.checkPassword;
+    const passwordRegex = /^(?=.*[^A-Za-z0-9])[^\s(){}\[\]'"]{8,20}$/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    // console.log(joinFrom);
+
+    if (joinFrom.name === "") {
+      alert("이름을 입력해주세요.");
+    } else if (!passwordRegex.test(joinFrom.password)) {
+      alert("8~20자 사이, 괄호, 따옴표를 제외한 특수문자를 포함해야합니다.");
+    } else if (!guardPw) {
+      setCheckPw(true);
+    } else if (!emailRegex.test(joinFrom.email)) {
+      alert("이메일을 올바르게 작성해주세요.");
+    } else {
+      setCheckPw(false);
+      const { checkPassword, ...joinData } = joinFrom;
+
+      axios.post("http://localhost:8000/users", joinData).then((res) => {
+        setModalContent(res.data);
+      });
+    }
+  };
 
   return (
     <div className="join_container">
@@ -30,19 +72,12 @@ const Join = () => {
             type="text"
             name="name"
           />
-          <label>닉네임</label>
+          <label>이메일</label>
           <Input
             onChange={handleChangeJoin}
-            placeholder="닉네임을 입력해주세요."
-            type="text"
-            name="nickname"
-          />
-          <label>아이디</label>
-          <Input
-            onChange={handleChangeJoin}
-            placeholder="아이디를 입력해주세요."
-            type="text"
-            name="id"
+            placeholder="이메일을 입력해주세요."
+            type="email"
+            name="email"
           />
           <label>비밀번호</label>
           <Input
@@ -51,23 +86,36 @@ const Join = () => {
             type="password"
             name="password"
           />
-          <label>비밀번호</label>
+          <label>비밀번호 확인</label>
           <Input
             onChange={handleChangeJoin}
             placeholder="비밀번호를 입력해주세요."
             type="password"
             name="checkPassword"
           />
-          <label>전화번호</label>
-          <Input
-            onChange={handleChangeJoin}
-            placeholder="전화번호를 입력해주세요."
-            type="tel"
-            name="phone"
-          />
+          {checkPw === true ? <p>비밀번호가 일치하지 않습니다.</p> : null}
 
           <Button>회원가입</Button>
         </form>
+        {modalContent === undefined ? null : modalContent?.type === "error" ? (
+          <div className="join_modal">
+            <p>{modalContent?.error}</p>
+            <Button
+              onClick={() => {
+                setModalContent(undefined);
+              }}
+              width={248}
+              height={54}
+            >
+              확인
+            </Button>
+          </div>
+        ) : (
+          <div className="join_modal">
+            <p>회원가입을 축하드립니다.</p>
+            <Button>로그인하기</Button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -1,10 +1,13 @@
 'use client'
 import { px2vw } from "@/util/px2vw";
 import styled from "styled-components";
-
-export const Button = styled.button`
-    width: ${px2vw(466)};
-    height: ${px2vw(45)};
+type ButtonType ={
+    width? : number;
+    height? : number;
+}
+export const Button = styled.button<ButtonType>`
+    width: ${({width})=>(width? px2vw(width) :px2vw(466))};
+    height: ${({height})=>(height? px2vw(height) :px2vw(45))};
     background: #82B7F6;
     border-radius: 10px;
     color: #FFFFFF;
@@ -12,4 +15,5 @@ export const Button = styled.button`
     font-weight: 700;
     border: none;
     cursor: pointer;
+    margin-top: 20px;
 `
